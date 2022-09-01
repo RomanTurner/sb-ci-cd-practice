a5f333800d653c8ec90e024f9cfbf044fab1f56b
roman turner <roman.nturner@gmail.com>
2022-08-31 22:01:24 -0700

    oops, revert

--- src/Button.js
+++ src/Button.js
@@ -33,7 +33,7 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 10px solid red;
+  border: 0;
   font-size: 20px;
   border-radius: 3em;
   cursor: pointer;
@@ -52,9 +52,8 @@ const StyledButton = styled.button`
   opacity: 1;
   margin: 0;
   background: transparent;
-
-  /* font-size: ${(props) =>
-    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px; */
+  font-size: ${(props) =>
+    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px;
   font-weight: ${typography.weight.extrabold};
   line-height: 1;
 
